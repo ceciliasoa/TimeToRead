2320d5a43bd1564cd9867715f371931263f93fca
Cecilia Soares <ceciliasoares@MacBook-Pro-de-Cecilia.local>
2020-08-13 12:04:45 -0300

    First commit

---
+++ timeToRead/timeToRead/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timeToRead
+//
+//  Created by Cecilia Soares on 13/08/20.
+//  Copyright © 2020 Cecilia Soares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
